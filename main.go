be6c20a650882aefd04b0d6e00101741bc5616fa
Enrisen <2019120183@ub.edu.bz>
2025-02-04 10:05:15 -0600

    Second commit

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
-// create a simple hellow world program in go
+// implement a feature that displays a simple message when the user visits the root URL of the web application.
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
-func main() {
-	fmt.Println(greeting())
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is my first successfull go lang thingy!"))
 }
-
-func greeting() string {
-	return "Hellow World"
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	log.Print("starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
